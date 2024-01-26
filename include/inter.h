690a0fe2da3f1fd3a8c242771f9a0a1b5b88384a
Wouter Wijsman <wouter@gridscale.io>
2024-01-26 16:05:38 +0100

    Remove translation support
    It was not being used and caused compile issues for MacOS.

--- include/inter.h
+++ include/inter.h
@@ -1,12 +1,6 @@
 #ifndef _INTER_H_
 #define _INTER_H_
 
-#if defined(__vita__) || defined(_WIN32)
  #define _(x) x
-#else
- #include <libintl.h>
- #include <locale.h>
- #define _(x) gettext(x)
-#endif
 
 #endif /* _INTER_H_ */
